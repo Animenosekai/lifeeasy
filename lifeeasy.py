33a494c61c53e8c95c14df3f6d418cee808e89a3
Animenosekai <maxence.osawa@gmail.com>
2020-05-26 20:17:34 +0200

    Description for every function

--- lifeeasy.py
+++ lifeeasy.py
@@ -25,14 +25,24 @@ body = []
 
 ## SAME AS TIME.SLEEP()
 def sleep(seconds):
+    """
+    Blocks the function/Waits for a given number of second before resuming the function.
+    """
     time.sleep(seconds)
 
 ## SAME AS CLEAR COMMAND IN BASH
 def clear():
+    """
+    Clears the console.
+    """
     os.system('cls' if os.name == 'nt' else 'clear')
 
 ## CHANGE WORKING DIR
 def change_working_dir(new_dir):
+    """
+    Changes the working directory.
+    Returns 0 if succeded, 1 if failed.
+    """
     try:
         os.chdir(new_dir)
         return 0
@@ -40,19 +50,34 @@ def change_working_dir(new_dir):
         return 1
 
 def working_dir():
+    """
+    Returns the working directory.
+    """
     return os.getcwd()
 
 ## COMMANDS
 def command_output(command_list):
+    """
+    Executes a command and returns the output of the command.
+    """
     result = subprocess.check_output(command, universal_newlines=True)
     return result
 
 def command(command):
+    """
+    Executes a command and returns the response code.
+    """
     result = os.system(command)
     return result
 
 ## MAKE HTTP REQUESTS
 def request(url, type, parameters=None, data=None, headers=None, json_body=None):
+    """
+    Makes an HTTP request.
+    This function needs at least an url and the method (called type here).
+    Returns a value of type <Response>
+    See the requests module documentation to learn more about this response type.
+    """
     if type.lower() == 'get':
         r = requests.get(url=url, params=parameters, headers=headers)
         return r
@@ -82,25 +107,42 @@ def request(url, type, parameters=None, data=None, headers=None, json_body=None)
 
 # TODAY'S DATE
 def today():
+    """
+    Returns the current date.
+    """
     today = datetime.date.today()
     return str(today)
 
 # CURRENT TIME
 def current_time():
+    """
+    Returns the current time with the format HOURs:MINUTEs:SECONDs
+    """
     now = datetime.datetime.now()
     time = now.strftime("%H:%M:%S")
     return time
 
 # SYSTEM TIMEZONE
 def timezone():
+    """
+    Returns the timezone of the system.
+    """
     return time.strftime("%Z", time.gmtime())
 
 def hours_from_greenwich():
+    """
+    Returns the number of hours between greenwitch and the computer.
+    """
     hours = time.strftime("%z", time.gmtime())
     return hours[:3]
 
 ## DISPLAY ACTIONS
 def display_action(action_to_display, times=3, delay=0.2):
+    """
+    Used to display an action (three dots will be animated after your message)
+    Warning: this is a blocking function, which means the program will only continue when the message disappear.
+    To display a message with a non-blocking function, try display().
+    """
     for _ in range(times-1):
         clear()
         print(action_to_display + ".")
@@ -117,14 +159,25 @@ def display_action(action_to_display, times=3, delay=0.2):
 
 # SETTING VARIABLES
 def display_title(title_string):
+    """
+    Sets the title for the message to be displayed with display()
+    """
     global title
     title = title_string
 
 def display_body(body_list):
+    """
+    Sets the body for the message to be displayed with display()
+    The body needs to be a list with each element of the list being a line.
+    """
     global body
     body = body_list
 
 def display(wait=2, delay=0.1):
+    """
+    Displays a message with his title (set with the display_title() function) and his body (set with the display_body() function)
+    This message will be displayed in another thread and therefore will be non-blocking (code after this function will continue running normally)
+    """
     global multi_thread_display_waiting_time
     multi_thread_display_waiting_time = wait
     if stop_displaying == False:
@@ -146,7 +199,10 @@ def display(wait=2, delay=0.1):
         return 'Another instance is already running.'
 
 # STOPPING THE THREAD
-def stop_multi_thread_display():
+def stop_display():
+    """
+    Stops displaying the multi-threaded status message (if launch through the display() function)
+    """
     global stop_displaying
     if stop_displaying == False:
         stop_displaying = True
@@ -159,6 +215,9 @@ def stop_multi_thread_display():
 
 # MOVING A FILE
 def move(origin, destination):
+    """
+    Moves a given file to a given destination (you can use file_info too)
+    """
     indexes_of_slash = [i for i, ltr in enumerate(origin) if ltr == "\\"]
     number_of_iterations = 0
     for index in indexes_of_slash:
@@ -183,6 +242,9 @@ def move(origin, destination):
 
 # DELETING A FILE
 def delete(file):
+    """
+    Deletes a given file (you can use file_info too)
+    """
     indexes_of_slash = [i for i, ltr in enumerate(file) if ltr == "\\"]
     number_of_iterations = 0
     for index in indexes_of_slash:
@@ -208,6 +270,9 @@ def delete(file):
 
 # OPENING A FILE
 def open(file):
+    """
+    Opens a given file (you can use file_info too)
+    """
     indexes_of_slash = [i for i, ltr in enumerate(file) if ltr == "\\"]
     number_of_iterations = 0
     for index in indexes_of_slash:
@@ -246,11 +311,13 @@ def get_size_from_bytes(bytes, suffix="B"):
             return f"{bytes:.2f}{unit}{suffix}"
         bytes /= factor
 
-
 ## OPERATIONS
 
 # SUM
 def sum(numbers):
+    """
+    Returns the sum of the provided list of numbers
+    """
     final_result = numbers[0]
     list = numbers
     del list[0]
@@ -259,6 +326,9 @@ def sum(numbers):
     return final_result
 
 def substract(numbers):
+    """
+    Returns the substraction of the provided list of numbers
+    """
     final_result = numbers[0]
     list = numbers
     del list[0]
@@ -267,6 +337,9 @@ def substract(numbers):
     return final_result
 
 def multiply(numbers):
+    """
+    Returns the multiplication of the provided list of numbers
+    """
     final_result = numbers[0]
     list = numbers
     del list[0]
@@ -275,6 +348,9 @@ def multiply(numbers):
     return final_result
 
 def divide(numbers):
+    """
+    Returns the division of the provided list of numbers
+    """
     final_result = numbers[0]
     list = numbers
     del list[0]
@@ -289,77 +365,146 @@ def divide(numbers):
 ## SYSTEM AND HARDWARE INFO
 
 def system():
+    """
+    Returns the system name
+    """
     return platform.uname().system
 
 def node():
+    """
+    Returns the node name
+    """
     return platform.uname().node
 
 def release():
+    """
+    Returns the system release name
+    """
     return platform.uname().release
 
 def version():
+    """
+    Returns the system version
+    """
     return platform.uname().version
 
 def machine():
+    """
+    Returns the machine name
+    """
     return platform.uname().machine
 
 def processor():
+    """
+    Returns the processor (CPU) name
+    """
     return platform.uname().processor
 
 def boot_time():
+    """
+    Returns the boot time with the format YEAR/MONTH/DAY HOUR:MIN:SECOND
+    """
     boot_time_timestamp = psutil.boot_time()
     bt = datetime.datetime.fromtimestamp(boot_time_timestamp)
     return f"{bt.year}/{bt.month}/{bt.day} {bt.hour}:{bt.minute}:{bt.second}"
 
 def number_of_physical_cores():
+    """
+    Returns the number of physical CPU cores
+    """
     return psutil.cpu_count(logical=False)
 
 def number_of_cores():
+    """
+    Returns the total number of CPU cores
+    """
     return psutil.cpu_count(logical=True)
 
 def cpu_max_frequency():
+    """
+    Returns the processor maximum frequency
+    """
     return psutil.cpu_freq().max
 
 def cpu_min_frequency():
+    """
+    Returns the processor minimum frequency
+    """
     return psutil.cpu_freq().min
 
 def cpu_current_frequency():
+    """
+    Returns the current processor frequency
+    """
     return psutil.cpu_freq().current
 
 def cpu_usage_per_core():
+    """
+    Returns the dict of the percent of processor usage for each core 
+    """
     usage = {}
     for i, percentage in enumerate(psutil.cpu_percent(percpu=True)):
         usage["core" + i] = percentage
     return usage
 
 def cpu_usage():
+    """
+    Returns the processor usage in percentage
+    """
     return psutil.cpu_percent()
 
 def total_ram():
+    """
+    Returns the total RAM installed
+    """
     return get_size_from_bytes(psutil.virtual_memory().total)
 
 def available_ram():
+    """
+    Returns the available RAM
+    """
     return get_size_from_bytes(psutil.virtual_memory().available)
 
 def used_ram():
+    """
+    Returns the used RAM
+    """
     return get_size_from_bytes(psutil.virtual_memory().used)
 
 def used_ram_percentage():
+    """
+    Returns the used RAM in percentage
+    """
     return psutil.virtual_memory().percent
 
 def total_swap_memory():
+    """
+    Returns the SWAP memory if available
+    """
     return get_size_from_bytes(psutil.swap_memory().total)
 
 def free_swap_memory():
+    """
+    Returns the free SWAP memory if available
+    """
     return get_size_from_bytes(psutil.swap_memory().free)
 
 def used_swap_memory():
+    """
+    Returns the used SWAP memory if available
+    """
     return get_size_from_bytes(psutil.swap_memory().used)
 
 def used_swap_memory_percentage():
+    """
+    Returns the used SWAP memory in percentage if available
+    """
     return get_size_from_bytes(psutil.swap_memory().percent)
 
 def disks_infos():
+    """
+    Returns the a dict of infos for each disks
+    """
     partion_infos = {}
     results = {}
     partitions = psutil.disk_partitions()
@@ -382,9 +527,15 @@ def disks_infos():
     return results
 
 def disk_total_read():
+    """
+    Returns the total read for the disk
+    """
     return get_size_from_bytes(psutil.disk_io_counters().read_bytes)
 
 def disk_total_write():
+    """
+    Returns the total write for the disk
+    """
     return get_size_from_bytes(psutil.disk_io_counters().write_bytes)
 
 def ip_address():
@@ -397,6 +548,9 @@ def ip_address():
                     return(f"{address.address}")
 
 def number_of_network_interfaces():
+    """
+    Returns the number of network interfaces
+    """
     number = 0
     # get all network interfaces (virtual and physical)
     if_addrs = psutil.net_if_addrs()
@@ -406,7 +560,13 @@ def number_of_network_interfaces():
     return number
 
 def net_total_bytes_sent():
+    """
+    Returns the total bytes sent over the network
+    """
     return get_size_from_bytes(psutil.net_io_counters().bytes_sent)
                 
 def net_total_bytes_received():
+    """
+    Returns the total bytes received over the network
+    """
     return get_size_from_bytes(psutil.net_io_counters().bytes_recv)
