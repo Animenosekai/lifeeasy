e0b8ca52955cccd0107318dc48f316fe3e008075
Animenosekai <40539549+Animenosekai@users.noreply.github.com>
2020-05-30 19:31:44 +0200

    [delete] test file

--- test.py
+++
@@ -1,7 +0,0 @@
-import lifeeasy
-body = []
-lifeeasy.display_title('')
-lifeeasy.display(0.1,0.1)
-
-while True:
-    lifeeasy.display_body(['Bytes received: {}'.format(lifeeasy.net_total_bytes_received()), 'Bytes sent: {}'.format(lifeeasy.net_total_bytes_sent()), "", 'Ram usage: {}%'.format(lifeeasy.used_ram_percentage())])
